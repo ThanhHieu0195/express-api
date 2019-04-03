90cbdc34d245e74294dd0314ec79b078c8d2c77d
karrot <hieu.tran@viralworks.com>
2019-04-03 16:36:00 +0700

    a

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var express = require('express')
-var app = express()
- 
-app.get('/hello', function (req, res) {
-  res.send('Hello World')
-})
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3030);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
