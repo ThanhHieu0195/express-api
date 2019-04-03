4ded921f5aeef9a9c4cef152bd96341d36b74dfe
karrot <hieu.tran@viralworks.com>
2019-04-03 16:26:02 +0700

    add

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
  
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('', function (req, res) {
   res.send('Hello World')
 })
  
